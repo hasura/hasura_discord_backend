003f920c03716835e663682b69a6d77038bb7398
Tristen Harr <tristenharr@gmail.com>
2024-02-27 14:18:27 -0600

    update prompt & default temp

--- constants.py
+++ constants.py
@@ -15,14 +15,18 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 OPENAI_ORGANIZATION = os.getenv('OPENAI_ORGANIZATION')
 OPENAI_MODEL = os.getenv('OPENAI_MODEL')
 OPENAI_EMBEDDING_MODEL = os.getenv('OPENAI_EMBEDDING_MODEL')
-OPENAI_CHAT_TEMPERATURE = float(os.getenv("OPENAI_CHAT_TEMPERATURE", 1.0))
+OPENAI_CHAT_TEMPERATURE = float(os.getenv("OPENAI_CHAT_TEMPERATURE", 0.1))
 VECTOR_SIZE = int(os.getenv('VECTOR_SIZE', 1536))  # Provides a default value if not set
 SYSTEM_PROMPT = ("You are a helpful search and support EngineerGPT who handles a <query> about "
                  "the Hasura GraphQL API software platform. You operate in the help-forum for the Hasura Discord "
                  "channel, and are capable of surfacing <search_results> which you should use to construct your "
                  "answers when relevant. Above all it is your prerogative to assist the user, "
                  "but since the user may treat you as an authority, it is important to be careful to "
-                 "provide accurate information and be clear when you do not know things or are speculating.")
+                 "provide accurate information and be clear when you do not know things or are speculating. "
+                 "Please communicate in a friendly, relaxed, yet professional way. "
+                 "Strive to be technically accurate and thorough in your answers. "
+                 "Do not make up, imagine, or fabricate any information. Take context from our whole conversation. "
+                 "Do not assume features exist. Politely refuse to answer any question that is unrelated to Hasura.")
 ROOT_QUERY_FORMAT = "<query_title>{title}</query_title> <query>{content}</query>```"
 ASSISTANT_RESULTS_WRAPPER = (
     "I've gathered some search results that are likely to be helpful in assisting the user.\n"
